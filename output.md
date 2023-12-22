c0003338e38cdc9583f178bfd349cc48a759564d
Ragavendhiran_V1 <ragav.pari6@gmail.com>
2023-12-22 11:06:56 +0530

    Initial commit

---
+++ output.md
@@ -0,0 +1,6 @@
+#Image 1
+![img.png](img.png)
+#Image 2
+![img_1.png](img_1.png)
+#Image 3
+![img_2.png](img_2.png)
---
+++ png_create.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+# Create your plot (e.g., plt.plot(...), plt.bar(...), etc.)
+
+plt.savefig("output_plot.png")  # Specify the desired filename
